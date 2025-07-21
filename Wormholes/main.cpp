f64f1ea68ce44f0afa5f096e396f22fddc6dc21e
Aditya-2204 <william1binki@gmail.com>
2025-07-20 22:59:40 -0500

    Wormholes complete using DFS and brute force

--- Wormholes/main.cpp
+++ Wormholes/main.cpp
@@ -5,33 +5,60 @@ TASK: wormhole
 */
 
 #include <iostream>
-#include <set>
-#include <map>
+
+int n;
+int x[20] , y[20], next_wormhole[20], part[20], answer=0;
+bool paired[20];
 
 using namespace std;
+
+bool check(){
+    for (int start = 1; start <= n; ++start) {
+        int pos = start;
+        for (int steps = 0; steps < n; ++steps) {
+            pos = next_wormhole[part[pos]];
+            if (pos == 0) break; // Reached end of line
+        }
+        if (pos != 0) return true; // Cycle detected
+    }
+    return false;
+}
+
+void dfs() {
+    int f = 1;
+    while(paired[f] && f<=n) f++;
+    if(f == n+1){
+        if(check()) answer++;
+    } //check
+    for(int s=f+1;s<=n;s++) {
+        if(!paired[s]) {
+            paired[f] = paired[s] = true;
+            part[f] = s, part[s] = f;
+            dfs();
+            paired[f] = paired[s] = false;
+            part[f] =part[s] = 0;
+        }
+    }
+}
 
 int main(){
     freopen("wormhole.in","r",stdin);
     freopen("wormhole.out","w",stdout);
 
-    map<int, set<int>> wormholes; //X and Y
-
-    int N; cin >> N;
-
-    for(int i=0;i<N;i++){
-        int x, y;
-        cin >> x >> y;
-        wormholes[y].insert(x);
+    cin >> n;
+    for(int i=1;i<=n;i++){
+        cin >> x[i] >> y[i];
+        next_wormhole[i]=0;
+        paired[i]=0;
     }
-    int c=0;
-    for(auto& [y, x_set] : wormholes) {
-        if (x_set.size() > 1) {
-            auto prev = x_set.begin();
-            auto it = std::next(prev);
-            for (; it != x_set.end(); ++it, ++prev) {
-                c += *it - *prev - 1;
-            }
-        }
+
+    for(int i=1;i<=n;i++)
+    for(int j=1;j<=n;j++) if(i!=j && y[i]==y[j] && x[j] >= x[i] && (!next_wormhole[i] || x[j]<x[next_wormhole[i]])) {
+        next_wormhole[i]=j;
     }
-    cout << c << endl;
+    
+    dfs();
+
+    cout << answer << endl;
+
 }
