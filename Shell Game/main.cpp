3c2f31248a885f00edb6c07d3d308aa2d90e8088
Aditya-2204 <william1binki@gmail.com>
2025-08-08 10:20:06 -0500

    Restarting

--- Shell Game/main.cpp
+++ Shell Game/main.cpp
@@ -1,34 +1,41 @@
-#include <algorithm>
-#include <cstdio>
+#include <iostream>
 #include <vector>
 
-using std::vector;
+using namespace std;
 
-int main() {
-	freopen("shell.in", "r", stdin);
+int main(){
+    freopen("shell.in","r",stdin);
+    freopen("shell.out","w",stdout);
 
-	int n;
-	scanf("%d", &n);
 
-	// shell_at_pos[i] stores the label of the shell located at position i
-	vector<int> shell_at_pos(3);
-	// Place the shells down arbitrarily
-	for (int i = 0; i < 3; i++) { shell_at_pos[i] = i; }
 
-	// counter[i] stores the number of times the shell with label i was picked
-	vector<int> counter(3);
-	for (int i = 0; i < n; i++) {
-		int a, b, g;
-		scanf("%d %d %d", &a, &b, &g);
-		// Zero indexing: offset all positions by 1
-		a--, b--, g--;
+    int N; cin >> N;
+    vector<int> shells(3,0);
+    vector<pair<pair<int,int>,int>> swaps(N); //(a,b,g)
+    for(int i=0;i<N;i++){
+        cin >> swaps[i].first.first >> swaps[i].first.second >> swaps[i].second;
+        swaps[i].first.first--;
+        swaps[i].first.second--;
+        swaps[i].second--;
+    }
+    int score=0;
+    for(int i=0;i<3;i++){
+        int t_score=0;
+        shells[i]=1;
+        for(int j=0;j<N;j++){ //Swapping time - simulation round
+            int a=swaps[j].first.first;
+            int b=swaps[j].first.second;
+            int g=swaps[j].second;
+            swap(shells[a],shells[b]);
+            if(shells[g]==1) t_score++;
 
-		// Perform Bessie's swapping operation
-		std::swap(shell_at_pos[a], shell_at_pos[b]);
-		// Count the number of times Elsie guesses each particular shell
-		counter[shell_at_pos[g]]++;
-	}
-
-	freopen("shell.out", "w", stdout);
-	printf("%d\n", std::max({counter[0], counter[1], counter[2]}));
+        }
+        score=max(score,t_score);
+        shells[0]=0; //Reset for next possibe swap
+        shells[1]=0;
+        shells[2]=0;
+        
+    }
+    cout << score << '\n';
+    return 0;
 }
